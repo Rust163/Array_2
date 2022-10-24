5176fea76803a6fd99cf03f5c08b4c8e3161d511
Рустам <rustam19871203@gmail.com>
2022-10-24 14:29:56 +0400

    Массивы часть 2. Домашка

--- src/Main.java
+++ src/Main.java
@@ -1,73 +1,57 @@
 import java.util.Random;
 
-
 public class Main {
-
 
     public static void main(String[] args) {
         Random rand = new Random();
         int[] expensesArr = new int[31];
-        int[] daysArr = new int[32];
         int sum = 0;
-        int averageExpenses;
-        int max = expensesArr[0];
-        //int min = expensesArr[0];
+        int min = 200000;
+        int max = 0;
 
-        for (int i = 0; i < expensesArr.length; i++) {
+        for (int i = 0; i < expensesArr.length -1; i++) {
             expensesArr[i] = rand.nextInt(100_000) + 100_000;
-        }
-        for (int i = 0; i < daysArr.length - 1; i++) {
-            daysArr[i] = i;
-        }
-        for (int i = 1; i < daysArr.length - 1; i++) {
-            System.out.println("Расходы за " + daysArr[i] + " -ое число составляют " + expensesArr[i] + " тыс. рублей.");
-        }
-
-
-        System.out.println();//--------------------------------------------
-
-
-        for (int j : expensesArr) {
-            if (max < j) {
-                max = j;
-                System.out.println("Сумма минимальных расходов за день: " + max + " тыс. рублей");
-                break;
-            }
-        }
-
-
-        System.out.println();//--------------------------------------------------
-
-
-        for (int j : expensesArr) {
-            if (j > max) {
-                max = j;
-                System.out.println("Сумма максимальных расходов за день: " + max + " тыс. рублей");
-                break;
-            }
-        }
-
-        System.out.println();//----------------------------------------
-
-        for (int j : expensesArr) {
-            sum = 31 * j;
-            System.out.println("Сумма расходов за месяц составляет : " + sum + " тыс. рублей.");
-            break;
-        }
-
-        System.out.println();//---------------------------------------------
-
-
-        while (true) {
-            averageExpenses = sum / 30;
-            System.out.println("Средняя сумма трат за месяц составила:" + averageExpenses + " тыс. рублей.");
-            break;
         }
 
         System.out.println();//-----------------------------------------------
 
+
+        for (int i = 0; i < expensesArr.length -1; i++) {
+            if (expensesArr[i] > max) {
+                max = expensesArr[i];
+            }
+            if (expensesArr[i] < min) {
+                min = expensesArr[i];
+            }
+        }
+        System.out.println("Минимальная сумма за день: " + min + " . " + "Максимальная сумма за день: " + max);
+
+
+        System.out.println();//-----------------------------------------------
+
+
+        for (int i = 0; i < expensesArr.length -1; i++) {
+            sum = expensesArr[i] * 30;
+            System.out.println("Сумма расходов за месяц составляет : " + sum + " тыс. рублей.");
+            break;
+        }
+
+
+        System.out.println();//---------------------------------------------
+
+
+        for (int i = 0; i < expensesArr.length -1; i--) {
+            int averageExpenses = sum / 30;
+            System.out.println("Средняя сумма трат за месяц составила: " + averageExpenses + " тыс. рублей.");
+            break;
+        }
+
+
+        System.out.println();//-----------------------------------------------
+
+
         char[] reverseFullName = { 'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
-        for (int i = 10; i < reverseFullName.length; i--) {
+        for (int i = reverseFullName.length -1;i >= 0 ;i--) {
             System.out.print(reverseFullName[i]);
         }
     }
